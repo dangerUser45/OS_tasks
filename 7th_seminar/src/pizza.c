a15c8e81437707308d9a520ba927ead01a6c906f
Danil Momot <danya541@bk.ru>
2025-11-19 00:34:15 +0300

    Debuging pizza.c

--- 7th_seminar/src/pizza.c
+++ 7th_seminar/src/pizza.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
@@ -100,31 +101,98 @@ struct Context {
 
 enum condition {EMPTY, TAKEN, READY};
 
+const char*  print_cond(enum condition cond) {
+  switch(cond) {
+    case EMPTY: return "empty";
+    case READY: return "ready";
+    case TAKEN: return "taken";
+    default: fprintf(stderr, RED "ERROR!!!" RESET "\n");
+  }
+}
+
+/* FIXME */
+void dbg_print(const char* file, int line, struct Context* context, int num_human, const char* profession, const char* format, ... );
+
+#define DBG_PRINT(context, num_human, profession, format, ...) do { dbg_print(__FILE__, __LINE__, context, num_human, profession, format, ##__VA_ARGS__); } while(0); 
+  
+void dbg_print(const char* file, int line, struct Context* context, int num_human, const char* profession, const char* format, ... ) {
+  safe_sem_wait(context->sem_mutex); 
+
+  char buf[4096] = {};
+  va_list args;
+  va_start(args, format); 
+  vsprintf(buf, format, args); 
+  va_end(args); 
+
+  fprintf(stderr, GREY "-----------------------------------------\n"); 
+  fprintf(stderr, "%s:%d\n", file, line); 
+  fprintf(stderr, "I'm a %s №%d: my pid = %d\n", profession, num_human, getpid()); 
+  fprintf(stderr, "Current action: %s\n\n", buf); 
+  fprintf(stderr, "num_table   = %d\nnum_chiefs  = %d\nnum_courier = %d\n\n", context->num_tables, context->num_chiefs, context->num_couriers); 
+  fprintf(stderr, "length_table_space   = %zu\nlength_table_context = %zu\n\n", context->length_table_space, context->length_table_context); 
+
+  int empty = 0, ready = 0, mutex = 0;
+  sem_getvalue(context->sem_empty, &empty);
+  sem_getvalue(context->sem_ready, &ready);
+  sem_getvalue(context->sem_mutex, &mutex);
+
+
+  fprintf(stderr, "Semaphores:\n"); 
+  fprintf(stderr, "\tempty = %d\n", empty); 
+  fprintf(stderr, "\tready = %d\n", ready); 
+  fprintf(stderr, "\tmutex = %d\n\n", mutex); 
+
+  fprintf(stderr, "Shm context:\n\t№     Address         Value\n"); 
+  for(size_t i = 0; i < context->length_table_context / sizeof(int); ++i) { 
+    if(i == context->length_table_context / sizeof(int) - 1) { 
+      fprintf(stderr, "\t      %p '%d'", context->mem_table_context + i, context->mem_table_context[i]);
+      fprintf(stderr, " <-- current num_chiefs\n");
+    } 
+    else
+      fprintf(stderr, "\t%-5zu %p '%s'\n", i + 1, context->mem_table_context + i, print_cond(context->mem_table_context[i]));
+  }
+
+  fprintf(stderr, "\nShm space:\n\t№     Address         Value\n");
+  for(size_t i = 0; i < context->length_table_space / NUM_PIZZAS_INGRIDIENTS; ++i) { 
+    fprintf(stderr, "\t%-5zu %p ", i + 1, context->mem_table_space + i * NUM_PIZZAS_INGRIDIENTS); 
+    for(size_t j = 0; j < NUM_PIZZAS_INGRIDIENTS; ++j) 
+      fprintf(stderr, "'%c' ", context->mem_table_space[i * NUM_PIZZAS_INGRIDIENTS +j]); 
+    fprintf(stderr, "\n");
+  }
+
+  fprintf(stderr, "-----------------------------------------\n" RESET);
+  safe_sem_post(context->sem_mutex);
+}
+
 static void ctor(int argc, char** argv, struct Context* context);
 static void dtor(struct Context* context);
 static void check_values(int num_tables, int num_chiefs, int num_couruiers);
 
-static void launch_group(int num_people,const struct Context* context,
-                         void (* func_ptr)(const struct Context* context));
+static void launch_group(int num_people, struct Context* const context,
+                         void (* func_ptr)(struct Context* const context,
+                         int num_human));
 
-static void chief(const struct Context* context);
-static void cook_pizza(const struct Context* context, int num_table);
+static void chief(struct Context* const context, int num_human);
+static void cook_pizza(const struct Context* context,
+                       int num_table, int num_human);
 static int find_table(const struct Context* context, enum condition cond);
 
-static void courier(const struct Context* context);
-static void take_pizza(char* table);
+static void courier(struct Context* const context, int num_human);
+static void take_pizza(char* table, int num_human);
 static bool all_pizzas_taken(enum condition* tables, int num_tables);
+
+const char*  print_cond(enum condition cond);
 
 //--------------------------------------------------------------
 int main(int argc, char** argv) {
   struct Context context = {};
-  ctor(argc, argv, &context); $
+  ctor(argc, argv, &context); 
 
-  launch_group(context.num_chiefs, &context, chief); $
-  launch_group(context.num_couriers, &context, courier); $
+  launch_group(context.num_chiefs, &context, chief); 
+  launch_group(context.num_couriers, &context, courier); 
   
   for (int i = 0; i < context.num_chiefs + context.num_couriers; ++i) wait(NULL);
-  dtor(&context); $
+  dtor(&context); 
 }
 //--------------------------------------------------------------
 static void ctor(int argc, char** argv, struct Context* context) {
@@ -147,32 +215,17 @@ static void ctor(int argc, char** argv, struct Context* context) {
   sem_t* sem_mutex = safe_sem_open(NAME_MUTEX,     O_CREAT | O_EXCL | O_RDWR, 0666, 1);
 
   int shm_table_space = safe_shm_open(NAME_TABLE_SPACE, O_CREAT | O_EXCL | O_RDWR, 0666);
-  size_t length_table_space = NUM_PIZZAS_INGRIDIENTS * num_tables + 2;
+  size_t length_table_space = NUM_PIZZAS_INGRIDIENTS * num_tables;
   ftruncate(shm_table_space, length_table_space);
 
   int shm_table_context = safe_shm_open(NAME_TABLE_CONTEXT, O_CREAT | O_EXCL | O_RDWR, 0666);
-  size_t length_table_context = sizeof(int) * num_tables + 1;
+  size_t length_table_context = sizeof(int) * (num_tables + 1);
   ftruncate(shm_table_context, length_table_context);
 
   char* mem_table_space = safe_mmap(NULL, length_table_space, PROT_READ | PROT_WRITE, MAP_SHARED, shm_table_space, 0);
   enum condition* mem_table_context = safe_mmap(NULL, length_table_context, PROT_READ | PROT_WRITE, MAP_SHARED, shm_table_context, 0);
-  /* FIXME */ /* fprintf(stderr, "table memory = %p\n"
-    "length_table_space = %zu\n"
-    "max_address = %p\n", mem_table_space, length_table_space, mem_table_space + length_table_space); */
+  
   mem_table_context[num_tables] = num_chiefs;
-
-  fprintf(stderr, "num_tables = %d, num_chiefs = %d\n", num_tables, num_chiefs);
-  fprintf(stderr, "mem_table_context + num_tables = %p: \'%d\'\n", mem_table_context + num_tables, mem_table_context[num_tables]);
-
-  /* FIXME */ fprintf(stderr, "mem_shm_context_: %p\n", mem_table_context);
-  for(int i = 0; i < num_chiefs + 2; ++i) {
-    fprintf(stderr, "%p ", mem_table_context + i);
-  }
-  fprintf(stderr, "\n");
-  for(int i = 0; i < num_chiefs + 2; ++i) {
-    fprintf(stderr, "%-14d ", mem_table_context[i]);
-  }
-  fprintf(stderr, "\n");
  
   context->num_tables = num_tables;
   context->num_chiefs = num_chiefs;
@@ -230,32 +283,43 @@ static void dtor(struct Context* context) {
   safe_shm_unlink(NAME_TABLE_CONTEXT);
 }
 //--------------------------------------------------------------
-static void launch_group(int num_people, const struct Context* context,
-                         void (* func_ptr)(const struct Context* context)) {
-for(int i = 0; i < num_people; ++i) {
+static void launch_group(int num_people, struct Context* const context,
+                         void (* func_ptr)(struct Context* const context,
+                         int num_human)) {
+for(int i = 1; i < num_people + 1; ++i) {
     pid_t pid = safe_fork();
-    if(pid == 0) func_ptr(context);
+    if(pid == 0) func_ptr(context, i);
   }
 }
 //--------------------------------------------------------------
-static void chief(const struct Context* context) {
-  safe_sem_wait(context->sem_empty); $
-
-  safe_sem_wait(context->sem_mutex); $ // mutex: on
+static void chief(struct Context* const context, int num_human) {
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "chief", "Только родился");
+  safe_sem_wait(context->sem_empty); 
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "chief", "Сделал --empty");
+  
+  safe_sem_wait(context->sem_mutex);  // mutex: on
   int num_table = find_table(context, EMPTY);
   context->mem_table_context[num_table] = TAKEN;
-  safe_sem_post(context->sem_mutex); $ //mutex: off
+  safe_sem_post(context->sem_mutex);  //mutex: off
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "chief", "Нашёл стол (№%d) и изменил значение нужного стола на taken", num_table + 1);
   
-  cook_pizza(context, num_table); $
+  cook_pizza(context, num_table, num_human);
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "chief", "Приготовил на столе(№%d) пиццу", num_table + 1);
 
-  safe_sem_wait(context->sem_mutex); $ //mutex: on
+
+  safe_sem_wait(context->sem_mutex);  //mutex: on
   context->mem_table_context[num_table] = READY;
-  safe_sem_post(context->sem_ready); $
-  safe_sem_post(context->sem_mutex); $ //mutex: off
-
-  safe_sem_wait(context->sem_mutex); $ //mutex: on
+  safe_sem_post(context->sem_ready); 
+  safe_sem_post(context->sem_mutex);  //mutex: off
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "chief", "поставил значение на столе (№%d) на READY и сделал ++ready", num_table + 1);
+  
+  safe_sem_wait(context->sem_mutex);  //mutex: on
   --context->mem_table_context[context->num_tables];
-  safe_sem_post(context->sem_mutex); $ //mutex: off
+  safe_sem_post(context->sem_mutex);  //mutex: off
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "chief", "Уменьшил значение живых поваров");
+
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "chief", "Прям перед смертью");
+  /* FIXME: debug */ fprintf(stderr, BLUE "Chief №%d exited" RESET "\n", num_human);
 
   exit(EXIT_SUCCESS);
 }
@@ -268,40 +332,53 @@ static int find_table(const struct Context* context, enum condition cond) {
   return -1;
 }
 //--------------------------------------------------------------
-static void cook_pizza(const struct Context* context, int num_table) {
+static void cook_pizza(const struct Context* context,
+                       int num_table, int num_human) {
   char* mem = context->mem_table_space;
   memcpy(mem + num_table * NUM_PIZZAS_INGRIDIENTS, "pizza", 5);
   
   // work immitation 
-  $ printf(ORANGE "I'm a chief and i cooked pizza!" RESET "\n"); fflush(stdout);
+   printf(ORANGE "I'm a chief №%d and i cooked pizza!" RESET "\n", num_human);
+  fflush(stdout);
+
   usleep(500000);
 }
 //--------------------------------------------------------------
-static void courier(const struct Context* context) {
+static void courier(struct Context* const context, int num_human) {
   char* mem_table_space = context->mem_table_space;
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "courier", "Только что родился");
 
-  safe_sem_wait(context->sem_mutex); $ //mutex: on
+  safe_sem_wait(context->sem_mutex);  //mutex: on
   int current_num_chiefs = context->mem_table_context[context->num_tables];
-  safe_sem_post(context->sem_mutex); $ //mutex: off
+  safe_sem_post(context->sem_mutex);  //mutex: off
 
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "courier", "Прям циклом while");
   while(current_num_chiefs != 0
         /*|| !all_pizzas_taken(context->mem_table_context, context->num_tables)*/) {
-  safe_sem_wait(context->sem_ready); $
-   /* FIXME */ fprintf(stderr, "current chiefs num = %d\n", current_num_chiefs);
   
-  safe_sem_wait(context->sem_mutex); $ //mutex: on
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "courier", "Жду перед ready-семафоромыы");
+  safe_sem_wait(context->sem_ready);
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "courier", "Сделал --ready");
+
+  
+  safe_sem_wait(context->sem_mutex);  //mutex: on
   int num_table = find_table(context, READY);
   context->mem_table_context[num_table] = TAKEN;
-  safe_sem_post(context->sem_mutex); $ //mutex: off
+  safe_sem_post(context->sem_mutex);  //mutex: off
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "courier", "Нашёл стол с готовой пиццей (№%d) и поставил туда значение taken", num_table);
 
-  take_pizza(mem_table_space + num_table * NUM_PIZZAS_INGRIDIENTS); $
+  take_pizza(mem_table_space + num_table * NUM_PIZZAS_INGRIDIENTS, num_human); 
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "courier", " Взял готовую пиццу со стола (№%d)", num_table);
+  
 
-  safe_sem_wait(context->sem_mutex); $ //mutex: on
-  context->mem_table_context[num_table] = EMPTY;  
+  safe_sem_wait(context->sem_mutex);  //mutex: on
+  context->mem_table_context[num_table] = EMPTY;
   safe_sem_post(context->sem_empty);
-  safe_sem_post(context->sem_mutex); $ //mutex: off
+  safe_sem_post(context->sem_mutex);  //mutex: off
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "courier", "Поставил значение стола опять на empty; иду снова на цикл");
 
   current_num_chiefs = context->mem_table_context[context->num_tables];
+  
 
   //   /* FIXME */ fprintf(stderr, GREY "mem_shm_context_: %p\n", context->mem_table_context);
   // for(int i = 0; i < context->num_chiefs + 2; ++i) {
@@ -313,9 +390,11 @@ static void courier(const struct Context* context) {
   // }
   // fprintf(stderr, RESET "\n");
 
-
   }
 
+  /* FIXME: debug */ DBG_PRINT(context, num_human, "courier", "Прямо перед смертью");
+
+  /* FIXME: debug */ fprintf(stderr, RED "Courier №%d exited" RESET "\n", num_human);
   exit(EXIT_SUCCESS);
 }
 //--------------------------------------------------------------
@@ -324,13 +403,15 @@ static bool all_pizzas_taken(enum condition* tables, int num_tables) {
   return true;
 }
 //--------------------------------------------------------------
-static void take_pizza(char* table) {
+static void take_pizza(char* table, int num_human) {
   if(strncmp(table, "pizza", NUM_PIZZAS_INGRIDIENTS)) {
     fprintf(stderr, RED "Error: chief cooked not a pizza!!!\n" RESET);
     exit(EXIT_FAILURE);
   }
-  // action immitation
-  $ printf(YELLOW "I'm a courier and i taked the pizza!" RESET "\n"); fflush(stdout);
+  // work immitation
+   printf(YELLOW "I'm a courier №%d and i taked the pizza!" RESET "\n", num_human);
+  fflush(stdout);
+  
   usleep(500000);
 }
 //--------------------------------------------------------------
